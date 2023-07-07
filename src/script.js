3d6495248121d189dd7b8da91beb1f8eed3df51b
Igor Milosavljevic <migor@live.co.uk>
2023-07-07 21:57:57 +0100

    update

---
+++ src/script.js
@@ -0,0 +1,8 @@
+let dice1 = "src/assets/images/dice1.png";
+let dice2 = "src/assets/images/dice2.png";
+let dice3 = "src/assets/images/dice3.png";
+let dice4 = "src/assets/images/dice4.png";
+let dice5 = "src/assets/images/dice5.png";
+let dice6 = "src/assets/images/dice6.png";
+
+diceList = [dice1, dice2, dice3, dice4, dice5, dice6];
