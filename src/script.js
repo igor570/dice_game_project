d018445aedea00324e930546f4816e46c05f8a6b
Igor Milosavljevic <migor@live.co.uk>
2023-07-07 23:03:07 +0100

    logic complete

--- src/script.js
+++ src/script.js
@@ -1,8 +1,17 @@
-let dice1 = "src/assets/images/dice1.png";
-let dice2 = "src/assets/images/dice2.png";
-let dice3 = "src/assets/images/dice3.png";
-let dice4 = "src/assets/images/dice4.png";
-let dice5 = "src/assets/images/dice5.png";
-let dice6 = "src/assets/images/dice6.png";
+const heading = document.querySelector(".heading");
+const dice = `assets/images/dice${Math.floor(Math.random() * 6) + 1}.png`;
+const dice2 = `assets/images/dice${Math.floor(Math.random() * 6) + 1}.png`;
 
-diceList = [dice1, dice2, dice3, dice4, dice5, dice6];
+const imgElement = document.querySelector("#diceImage");
+const imgElement2 = document.querySelector("#diceImage2");
+
+const player1Result = (imgElement.src = dice);
+const player2Result = (imgElement2.src = dice2);
+
+if (player1Result > player2Result) {
+  heading.innerHTML = "Player 1 has Won";
+} else if (player1Result === player2Result) {
+  heading.innerHTML = "Players have tied";
+} else {
+  heading.innerHTML = "Player 2 has Won";
+}
