da94cc2a3290a355288374caaa2c6d316b8df1f9
Igor Milosavljevic <migor@live.co.uk>
2023-07-08 13:24:32 +0100

    logic updated

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const heading = document.querySelector(".heading");
-const dice = `assets/images/dice${Math.floor(Math.random() * 6) + 1}.png`;
-const dice2 = `assets/images/dice${Math.floor(Math.random() * 6) + 1}.png`;
+const playerRoll = Math.floor(Math.random() * 6) + 1;
+const playerRoll2 = Math.floor(Math.random() * 6) + 1;
+const dice = `assets/images/dice${playerRoll}.png`;
+const dice2 = `assets/images/dice${playerRoll2}.png`;
 
 const imgElement = document.querySelector("#diceImage");
 const imgElement2 = document.querySelector("#diceImage2");
@@ -8,9 +10,9 @@ const imgElement2 = document.querySelector("#diceImage2");
 const player1Result = (imgElement.src = dice);
 const player2Result = (imgElement2.src = dice2);
 
-if (player1Result > player2Result) {
+if (playerRoll > playerRoll2) {
   heading.innerHTML = "Player 1 has Won";
-} else if (player1Result === player2Result) {
+} else if (playerRoll === playerRoll2) {
   heading.innerHTML = "Players have tied";
 } else {
   heading.innerHTML = "Player 2 has Won";
